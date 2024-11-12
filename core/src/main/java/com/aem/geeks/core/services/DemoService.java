503ba7c74f35149da7511a3db40266da0c75cee9
lmuppidi <44202516+lmuppidi@users.noreply.github.com>
2024-11-12 11:34:29 -0600

    Update DemoService.java

--- core/src/main/java/com/aem/geeks/core/services/DemoService.java
+++ core/src/main/java/com/aem/geeks/core/services/DemoService.java
@@ -3,6 +3,7 @@ package com.aem.geeks.core.services;
 import com.day.cq.wcm.api.Page;
 
 import java.util.Iterator;
+/** Returns Pages**/
 
 public interface DemoService {
     public Iterator<Page> getPages();
